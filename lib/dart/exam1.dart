c24d9d10ac5c8d4588b6df41553ee2b18b821a78
parkgyeonghwan <ghksdlajwu@gmail.com>
2021-11-19 21:45:16 +0900

    11.19실습

---
+++ lib/dart/exam1.dart
@@ -0,0 +1,18 @@
+void main() {
+  oddeven(30, 15);
+  oddeven(12, 40);
+  oddeven(2,1);
+  oddeven(1, 2);
+  oddeven(1, 1);
+  oddeven(0,1);
+}
+
+int oddeven(int n, int m) {
+  if (n % 2 == 0 && m % 2 == 1) {
+    print("YES");
+  } else if (m % 2 == 0 && n % 2 == 1) {
+    print("YES");
+  } else {
+    print("NO");
+  }
+}
