d48a60f0b207e85d06f49cbea9dfc33267041986
parkgyeonghwan <ghksdlajwu@gmail.com>
2021-11-24 14:14:47 +0900

    11.24 과제1

---
+++ lib/exma_11.24.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'dart:io';
+
+void main() {
+  List<String> b = stdin.readLineSync(encoding: utf8).split('');
+  print(sibal(b));
+}
+
+int sibal(List<String> b) {
+  int num = 0;
+
+  for (int i = 0; i < b.length; i++) {
+    if (b[i] == '<') {
+      num += 10;
+    } else if (b[i] == '/') {
+      num += 1;
+    }
+  }
+
+  return (num);
+}
